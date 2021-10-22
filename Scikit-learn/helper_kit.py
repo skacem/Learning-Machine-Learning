d93b522dc20fd0d9716db1a840a13676a0f49cc8
Skander Kacem <25346686+skacem@users.noreply.github.com>
2021-10-22 11:36:26 +0200

    helper functions

---
+++ Scikit-learn/helper_kit.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def plot_decision_function(fitted_classifier, range_features, ax=None):
+    """Plot the boundary of the decision function of a classifier."""
+    from sklearn.preprocessing import LabelEncoder
+
+    feature_names = list(range_features.keys())
+    # create a grid to evaluate all possible samples
+    plot_step = 0.02
+    xx, yy = np.meshgrid(
+        np.arange(*range_features[feature_names[0]], plot_step),
+        np.arange(*range_features[feature_names[1]], plot_step),
+    )
+
+    # compute the associated prediction
+    Z = fitted_classifier.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = LabelEncoder().fit_transform(Z)
+    Z = Z.reshape(xx.shape)
+
+    # make the plot of the boundary and the data samples
+    if ax is None:
+        _, ax = plt.subplots()
+    ax.contourf(xx, yy, Z, alpha=0.4, cmap="RdBu")
+
+    return ax
